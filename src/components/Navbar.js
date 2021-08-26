3381bf8f29b054bdd3249a246ea9e3f4450415f2
BrianTheLion328 <bmoniz328@gmail.com>
2021-08-26 13:24:31 -0700

    added TF logos and graphics

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
     return (
         <div className="navbar">
+            <img className="texasLogo" src="https://boombabyhome.files.wordpress.com/2015/04/60109-texas-frightmare-weekend-feature.jpg"></img>
             <Link to="/celebrities">
                 <button className="navButton">Celebrities</button>
             </Link>
