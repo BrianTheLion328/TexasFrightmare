3771c9859ab4539ea29a6f7a925ea218ddb9f36e
BrianTheLion328 <bmoniz328@gmail.com>
2021-08-26 21:45:10 -0700

    changed stylings on welcome page

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const apiRouter = require('express').Router();
 
 apiRouter.get("/", (req, res, next) => {
     res.send({
-        message: "API is under construction!"
+        message: "API is still under construction!"
     });
 });
 
